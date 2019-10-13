ad4e8d2c2f405a7fe5cc773f806f6a9da8aab323
florencia <aflorenciacabrera@gmail.com>
2019-10-12 23:05:54 -0300

    login frontend

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/Login';
+import { Container } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+    <Login></Login> 
+    </Container>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+      
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/Login.tsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, SafeAreaView, TextInput } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label, Text, Separator , Button} from 'native-base';
+
+export default class Login extends Component {
+    handleSubmit = () =>{
+
+    }
+  render() {
+    return (
+     
+        <View style={styles.container}> 
+        <Content>
+        <Form >
+        
+          <Item floatingLabel last  > 
+          <Label >DNI</Label>
+          <Input keyboardType={'numeric'} />
+          </Item>
+          <Item floatingLabel last >
+          <Label >LU</Label>
+          <Input keyboardType={'numeric'} />
+          </Item>
+          {/* <TextInput  
+          placeholder="DNI"  
+          
+          underlineColorAndroid='transparent' 
+          style={styles.TextInputStyle}   
+          keyboardType={'numeric'}
+          />   */}
+            {/* <Item floatingLabel last>
+              <Label >DNI</Label>
+              <Input />
+            </Item>
+
+            <Item floatingLabel last>
+              <Label>LU</Label>
+              <Input />
+            </Item> */}
+              <Text>  </Text>
+
+            <Button  style={{  width:"30%" }} >
+                <Text>Aceptar</Text>
+            </Button>
+            </Form>
+        </Content>
+        </View>
+     
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+    justifyContent: 'center',  
+    marginTop: 50,
+      padding: 40,
+     backgroundColor: '#fff',
+    //  alignItems: 'center',
+   width:"100%"
+
+  },
+  headerText: {  
+    fontSize: 20,  
+    textAlign: "center",  
+    margin: 10,  
+    fontWeight: "bold"  
+},  
+TextInputStyle: {  
+    textAlign: 'center',  
+    height: 40,  
+    borderRadius: 10,  
+    borderWidth: 2,  
+    borderColor: '#009688',  
+    marginBottom: 10  
+},
+});
