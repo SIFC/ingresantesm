f6f8a1560aff038c5c41049071d1b16101906a37
Florencia Cabrera <fcabrera@invico.gov.ar>
2019-11-08 12:28:49 -0300

    taller

--- src/Taller.tsx
+++ src/Taller.tsx
@@ -24,7 +24,7 @@
 // ---------------------------------------------------------------------
 
 import React, { Component } from 'react';
-import { Container, Content, List, ListItem, Text, Separator, Button, H2, H3, H1, } from 'native-base';
+import { Container, Content, List, ListItem, Text, Separator, Button, H2, H3, H1, Left, Right, Icon, } from 'native-base';
 import Footer from './components/Footer'
 import Header from './components/Header'
 interface Props {
@@ -46,37 +46,32 @@ export default class Taller extends Component <Props>{
     // const { params } = this.props.navigation.state;
      const { navigation } = this.props;
      const datosGenerales = navigation.getParam('datosGenerales');
+
     return (
       <Container>
           {/* itemDivider */}
           {/* <Separator bordered>
             <Text>MIDFIELD</Text>
           </Separator> */}
-          
-          <ListItem last>
-              <Button rounded light onPress={() => this.props.navigation.navigate('Taller_detalles')}>
-                <Text>  {datosGenerales.name}</Text>
-              </Button>
-          </ListItem>
-          
-          <ListItem last>
-              <Button rounded light onPress={() => alert("Talleres 2")}>
-                <Text>Taller 2</Text>
-              </Button>
-          </ListItem>   
-                         
-          <ListItem last>
-              <Button rounded light onPress={() => alert("Talleres 1")}>
-                <Text>Taller 1</Text>
-              </Button>
-          </ListItem>
-         
-          <ListItem last>
-              <Button rounded light onPress={() => alert("Talleres 2")}>
-                <Text>Taller 2</Text>
-              </Button>
-          </ListItem>
+          <List  dataArray={datosGenerales.talleres}
+              renderRow={(item) =>
+              <ListItem  last  onPress={() => this.props.navigation.navigate('Taller_detalles')} >
+                {/* <Button rounded light > */}
+                <Left>
+                <Text>  {item.titulo}</Text></Left>
+              {/* </Button> */}
+               <Right>
+                 <Icon name="arrow-forward"></Icon>
+               </Right>
+              </ListItem>}>
 
+          {/* <ListItem last>
+              <Button rounded light onPress={() => this.props.navigation.navigate('Taller_detalles')}>
+                <Text>  {datosGenerales.talleres.titulo}</Text>
+              </Button>
+          </ListItem> */}
+          </List>
+         
         <Footer></Footer>
 
      </Container>
